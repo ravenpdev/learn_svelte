58c8c83cecc57c0dcf50e629e0f737f5b553d38b
RavenP <ravenp.dev@gmail.com>
2023-03-07 03:24:40 +0800

    svelte props and restProps

---
+++ src/lib/components/CounterWithProps.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let initialCountValue = 0;
+  export let maxCountValue = undefined;
+  // export let maxCountValue = undefined;
+
+  console.log($$props);
+  console.log($$restProps);
+
+  let count = initialCountValue;
+
+  function increment() {
+    if (count === maxCountValue) return;
+
+    count += 1;
+  }
+</script>
+
+<div>
+  <h1>{count}</h1>
+
+  <button on:click={increment}>Clicks {count}</button>
+</div>
---
+++ src/lib/containers/ComponentWithPropsCountContainer.svelte
@@ -0,0 +1,26 @@
+<script>
+  import CounterWithProps from "../components/CounterWithProps.svelte";
+
+  // const initialCountValue = 3;
+  // const maxCountValue = 10;
+
+  const props = {
+    initialCountValue: 0,
+    maxCountValue: 10,
+  };
+</script>
+
+<section>
+  <!-- spread props -->
+  <CounterWithProps
+    {...props}
+    x="x"
+    y="y"
+    person={{ name: "raven", age: 30 }}
+    another={0}
+  />
+
+  <!-- <CounterWithProps {initialCountValue} {maxCountValue} /> -->
+  <!-- <CounterWithProps initialCountValue={3} /> -->
+  <!-- <CounterWithProps initialCountValue={3} maxCountValue={10} /> -->
+</section>
