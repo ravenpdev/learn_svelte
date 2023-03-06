bd13262a04e2b7a75b687d90ec42f28e2b65c5df
RavenP <ravenp.dev@gmail.com>
2023-03-07 03:21:58 +0800

    basic svelte component

---
+++ src/lib/components/Basic.svelte
@@ -0,0 +1,18 @@
+<script>
+  // your script goes here
+  let name = "Svelte";
+  let src = "https://picsum.photos/200/200";
+  let string = "Some <strong>string</strong>";
+</script>
+
+<img {src} alt="Some text {name}" />
+<h1>Hello {name.toUpperCase()}</h1>
+<p>
+  {@html string}
+</p>
+
+<style>
+  /* h1 { */
+  /*   color: #ff0000; */
+  /* } */
+</style>
