4c0b0e4cf81c0f8a8ff31fafb07bfac26031f024
RavenP <ravenp.dev@gmail.com>
2023-03-07 15:56:02 +0800

    using named slot and default slot props

--- src/lib/containers/UsingSlotContainer.svelte
+++ src/lib/containers/UsingSlotContainer.svelte
@@ -1,5 +1,11 @@
 <script>
-  import Button from "../components/slot/Button.svelte";
+  import Button from "../components/Button.svelte";
+  import {
+    FaAddressBook,
+    FaAddressCard,
+    FaAd,
+    FaAngellist,
+  } from "svelte-icons/fa";
 </script>
 
 <section>
@@ -7,22 +13,46 @@
 
   <div>
     <h2>Variant Text</h2>
-    <Button size="small" color="primary">Small</Button>
-    <Button color="primary">Regular</Button>
-    <Button size="large" color="primary">Large</Button>
+    <div class="flex gap-2">
+      <!-- slot props -->
+      <Button let:isLeftHovered size="small" color="primary" shadow>
+        <div slot="leftContent" class="w-5 h-5">
+          {#if isLeftHovered}
+            <FaAddressCard />
+          {:else}
+            <FaAddressBook />
+          {/if}
+        </div>
+        Small
+        <span slot="rightContent" class="h-5 w-5"><FaAd /></span>
+      </Button>
+
+      <Button color="primary" shadow>
+        <div slot="leftContent" class="h-5 w-5"><FaAngellist /></div>
+        Regular
+      </Button>
+
+      <Button size="large" color="primary" shadow>Large</Button>
+    </div>
   </div>
 
   <div>
     <h2>Outlined Text</h2>
-    <Button variant="outlined" size="small" color="primary">Small</Button>
-    <Button variant="outlined" color="primary">Regular</Button>
-    <Button variant="outlined" size="large" color="primary">Large</Button>
+
+    <div class="flex gap-2">
+      <Button variant="outlined" size="small" color="primary">Small</Button>
+      <Button variant="outlined" color="primary">Regular</Button>
+      <Button variant="outlined" size="large" color="primary">Large</Button>
+    </div>
   </div>
 
   <div>
     <h2>Filled Text</h2>
-    <Button variant="filled" size="small" color="primary">Small</Button>
-    <Button variant="filled" color="primary">Regular</Button>
-    <Button variant="filled" size="large" color="primary">Large</Button>
+
+    <div class="flex gap-2">
+      <Button variant="filled" size="small" color="primary">Small</Button>
+      <Button variant="filled" color="primary">Regular</Button>
+      <Button variant="filled" size="large" color="primary">Large</Button>
+    </div>
   </div>
 </section>
