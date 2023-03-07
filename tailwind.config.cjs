499d5992330c5406dcb9ad562bfba7d2d9d0dfc5
RavenP <ravenp.dev@gmail.com>
2023-03-07 08:59:13 +0800

    create custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
+    colors: {
+      primary: colors.purple,
+      secondary: colors.pink,
+      info: colors.sky,
+      success: colors.teal,
+      warning: colors.yellow,
+      error: colors.red,
+      zinc: colors.zinc,
+    },
   },
   plugins: [],
 };
