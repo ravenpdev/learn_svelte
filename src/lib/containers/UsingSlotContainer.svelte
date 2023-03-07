d1da1cc3cbdc0b26afd6e40e1b5e0481936e9537
RavenP <ravenp.dev@gmail.com>
2023-03-07 08:58:51 +0800

    create button with text, filled, and outlined variant

--- src/lib/containers/UsingSlotContainer.svelte
+++ src/lib/containers/UsingSlotContainer.svelte
@@ -5,6 +5,24 @@
 <section>
   <h1>Using Slot</h1>
 
-  <Button />
-  <!-- <Button>My Button</Button> -->
+  <div>
+    <h2>Variant Text</h2>
+    <Button size="small" color="primary">Small</Button>
+    <Button color="primary">Regular</Button>
+    <Button size="large" color="primary">Large</Button>
+  </div>
+
+  <div>
+    <h2>Outlined Text</h2>
+    <Button variant="outlined" size="small" color="primary">Small</Button>
+    <Button variant="outlined" color="primary">Regular</Button>
+    <Button variant="outlined" size="large" color="primary">Large</Button>
+  </div>
+
+  <div>
+    <h2>Filled Text</h2>
+    <Button variant="filled" size="small" color="primary">Small</Button>
+    <Button variant="filled" color="primary">Regular</Button>
+    <Button variant="filled" size="large" color="primary">Large</Button>
+  </div>
 </section>
